0b46c76fd06f4efbcd8a049528bea064dd994c13
eDusVx <eduardosvx@gmail.com>
2022-07-10 12:23:47 -0300

    Modificando testa_velha.py

--- testa_velha.py
+++ testa_velha.py
@@ -1,7 +1,10 @@
 """Teste jogo da velha"""
+import numpy as np
 import velha
 
 
 def test_existe_ganhador():
-    """Função que testa de existe algum ganhador"""
+    """Função que testa de existe algum vencedor"""
     assert velha.check_win() == True
+
+
--- velha.py
+++ velha.py
@@ -3,18 +3,60 @@ import numpy as np
 
 
 def check_win():
-    """Função para checar ganhador"""
-    if any(np.sum(board, 0) == 6) or any(np.sum(board, 1) == 6):
-        return True
-    if any(np.sum(board, 0) == 3) or any(np.sum(board, 1) == 3):
-        return True
+    """Função para checar vencedor"""
+    if 0 not in board[..., 0]:
+        if any(np.sum(board, 0) == 6):
+            vencedor = 2
+            resultado(vencedor)
+            return True
+        if any(np.sum(board, 0) == 3):
+            vencedor = 1
+            resultado(vencedor)
+            return True
+    if 0 not in board[0]:
+        if any(np.sum(board, 1) == 6):
+            vencedor = 2
+            resultado(vencedor)
+            return True
+        if any(np.sum(board, 1) == 3):
+            vencedor = 1
+            resultado(vencedor)
+            return True
+    if 0 not in np.diag(board[::1]):
+        if sum(np.diag(board[::1])) == 6:
+            vencedor = 2
+            resultado(vencedor)
+            return True
+        if sum(np.diag(board[::1])) == 3:
+            vencedor = 1
+            resultado(vencedor)
+            return True
+    if 0 not in np.diag(board[::-1]):
+        if sum(np.diag(board[::-1])) == 6:
+            vencedor = 2
+            resultado(vencedor)
+            return True
+        if sum(np.diag(board[::-1])) == 3:
+            vencedor = 1
+            resultado(vencedor)
+            return True
+    checar_empate()
     return False
+def checar_empate():
+    """função para cehcar empate"""
+    empate = 0 in board
+    if empate == False:
+        print(f'O jogo terminou empatado')
 
 
-board = np.array([[2, 2, 0], [2, 0, 0], [1, 0, 1]])
+def resultado(vencedor):
+    """função que define quem foi o vencedor"""
+    print(f'O vencedor foi o jogador {vencedor}')
+
+
+board = np.array([[2, 1, 1], [4, 1, 6], [7, 8, 1]])
 print(board)
 
-print(check_win())
+check_win()
 
-s = np.sum(board, 0) == 6
-print(s)
+
