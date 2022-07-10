c9d37208ea1b81c63d1d174325b8630e2dffde81
eDusVx <eduardosvx@gmail.com>
2022-07-10 20:33:19 -0300

    Modificando testa_velha.py

--- testa_velha.py
+++ testa_velha.py
@@ -2,6 +2,12 @@
 import velha
 
 
+def teste_checar_vencedor():
+    """testando a função que procura se há algum vencedor no momento"""
+    assert velha.checar_vencedor() == 1 \
+           or velha.checar_vencedor() == 2 or velha.checar_vencedor() == 0
+
+
 def teste_resultado():
     """testeando a função resultado, que torna 1 caso 'X' seja o vencedor,
     2 caso o 'O' seja o vencedor e '0' no caso de empate"""
@@ -9,11 +15,12 @@ def teste_resultado():
 
 
 def teste_indefinido():
-    """testando a função indefinido que retorna -1 se o resultado do jogo ainda está indefinido"""
-    assert velha.indefinido() == -1
+    """testando a função indefinido que retorna -1 se o resultado do jogo ainda está indefinido
+     ou None se já está definido"""
+    assert velha.indefinido() == -1 or velha.indefinido() is None
 
 
 def teste_impossivel():
     """testando a função impossivel que retorna - 2 se o resultado do jogo é impossível
-     ou está fora das regras"""
-    assert velha.impossivel() == -2
+    ou None se o resultado é possível ou indefinido"""
+    assert velha.impossivel() == -2 or velha.impossivel() is None
