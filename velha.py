fdd6d0d8ae5899d3b938614e05de895eea37b1b7
eDusVx <eduardosvx@gmail.com>
2022-07-10 20:21:34 -0300

    Modificando velha.py

--- velha.py
+++ velha.py
@@ -1,56 +1,48 @@
 """Jogo da velha TDD"""
-import numpy as np
+import numpy as np  # importando biblioteca
 
 
 def checar_vencedor():
     """Função que procura um vencedor para a partida"""
-    global VENCEDOR
-    VENCEDOR = 0
-    if 0 not in board[..., 0]:  # checando se há vencedores nas colunas na vertical
-        if any(np.sum(board, 0) == 6):
-            VENCEDOR = 2
-            resultado()
-        if any(np.sum(board, 0) == 3):
-            VENCEDOR = 1
-            resultado()
-    if 0 not in board[0]:  # checando se há vencedores nas colunas na horizontal
-        if any(np.sum(board, 1) == 6):
-            VENCEDOR = 2
-            resultado()
-        if any(np.sum(board, 1) == 3):
-            VENCEDOR = 1
-            resultado()
-    if 0 not in np.diag(board[::1]):  # checando se há vencedores na diagonal principal
-        if sum(np.diag(board[::1])) == 6:
-            VENCEDOR = 2
-            resultado()
-        if sum(np.diag(board[::1])) == 3:
-            VENCEDOR = 1
-            resultado()
-    if 0 not in np.diag(board[::-1]):  # checando se há vencedores na diagonal secundária
-        if sum(np.diag(board[::-1])) == 6:
-            VENCEDOR = 2
-            resultado()
-        if sum(np.diag(board[::-1])) == 3:
-            VENCEDOR = 1
-            resultado()
-    resultado()
+    vencedor = 0
+    for i in range(0, 2):
+        if 0 not in board[..., i] and np.sum(board, 0)[i] == 6:  # check win vertical
+            vencedor = 2
+        if 0 not in board[..., i] and np.sum(board, 0)[i] == 3:
+            vencedor = 1
+        if 0 not in board[i] and np.sum(board, 1)[i] == 6:  # check win horizontal
+            vencedor = 2
+        if 0 not in board[i] and np.sum(board, 1)[i] == 3:
+            vencedor = 1
+    if 0 not in np.diag(board[::1]) and sum(np.diag(board[::1])) == 6:  # check win diagonal 1
+        vencedor = 2
+    if 0 not in np.diag(board[::1]) and sum(np.diag(board[::1])) == 3:
+        vencedor = 1
+    if 0 not in np.diag(board[::-1]) and sum(np.diag(board[::-1])) == 6:  # check win diagonal 2
+        vencedor = 2
+    if 0 not in np.diag(board[::-1]) and sum(np.diag(board[::-1])) == 3:
+        vencedor = 1
+    return vencedor
 
 
 def resultado():
-    """função que define quem foi o vencedor"""
+    """função que define quem foi o vencedor ou se empatou"""
     empate = 0 in board
-    if empate is False and VENCEDOR == 0:
+    if empate is False and checar_vencedor() == 0:  # define se o jogo terminou empatado
         print('O jogo terminou empatado.')
-    elif VENCEDOR in (1, 2) and impossivel() != - 2:
-        print(f'O vencedor foi o jogador {VENCEDOR}.')
-    return VENCEDOR
+        return checar_vencedor()
+    if checar_vencedor() in (1, 2) and impossivel() != - 2:  # define o vencedor
+        print(f'O vencedor foi o jogador {checar_vencedor()}.')
+        return checar_vencedor()
+    indefinido()
+    return None
 
 
 def indefinido():
     """Função que define que o jogo ainda está em adamento"""
     empate = 0 in board
-    if empate is True and VENCEDOR == 0:
+    if empate is True and checar_vencedor() == 0:
+        print("o jogo está indefinido")
         return -1
     return None
 
@@ -60,11 +52,11 @@ def impossivel():
     qntd_o = np.count_nonzero(board == 1)
     qntd_x = np.count_nonzero(board == 2)
     if abs(qntd_o - qntd_x) != 1:
+        print("o jogo é impossível")
         return -2
     return None
 
 
-board = np.array([[2, 2, 1], [0, 2, 0], [1, 1, 1]])
+board = np.array([[2, 1, 1], [2, 1, 1], [2, 2, 1]])
 print(board)
-
-checar_vencedor()
+print(resultado())
