55e715bfda469f3e331c56c3907278053321fbf4
eDusVx <eduardosvx@gmail.com>
2022-07-10 18:16:43 -0300

    Modificando testa_velha.py

--- testa_velha.py
+++ testa_velha.py
@@ -8,4 +8,12 @@ def teste_resultado():
     assert velha.resultado() == 1 or velha.resultado() == 2 or velha.resultado() == 0
 
 
+def teste_indefinido():
+    """testando a função indefinido que retorna -1 se o resultado do jogo ainda está indefinido"""
+    assert velha.indefinido() == -1
 
+
+def teste_impossivel():
+    """testando a função impossivel que retorna - 2 se o resultado do jogo é impossível
+     ou está fora das regras"""
+    assert velha.impossivel() == -2
