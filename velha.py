67650efa87db371098f04caf678604cd5b1081b8
eDusVx <eduardosvx@gmail.com>
2022-07-12 12:45:13 -0300

    Modificando velha.py

--- velha.py
+++ velha.py
@@ -2,10 +2,10 @@
 import numpy as np  # importando biblioteca
 
 
-def checar_vencedor():
+def checar_resultado():
     """Função que procura um vencedor para a partida"""
     vencedor = 0
-    for i in range(0, 2):
+    for i in range(0,3):
         if 0 not in board[..., i] and np.sum(board, 0)[i] == 6:  # check win vertical
             vencedor = 2
         if 0 not in board[..., i] and np.sum(board, 0)[i] == 3:
@@ -28,12 +28,12 @@ def checar_vencedor():
 def resultado():
     """função que define quem foi o vencedor ou se empatou"""
     empate = 0 in board
-    if empate is False and checar_vencedor() == 0:  # define se o jogo terminou empatado
+    if empate is False and checar_resultado() == 0:  # define se o jogo terminou empatado
         print('O jogo terminou empatado.')
-        return checar_vencedor()
-    if checar_vencedor() in (1, 2) and impossivel() != - 2:  # define o vencedor
-        print(f'O vencedor foi o jogador {checar_vencedor()}.')
-        return checar_vencedor()
+        return checar_resultado()
+    if checar_resultado() in (1, 2) and impossivel() != - 2:  # define o vencedor
+        print(f'O vencedor foi o jogador {checar_resultado()}.')
+        return checar_resultado()
     indefinido()
     return None
 
@@ -41,7 +41,7 @@ def resultado():
 def indefinido():
     """Função que define que o jogo ainda está em adamento"""
     empate = 0 in board
-    if empate is True and checar_vencedor() == 0:
+    if empate is True and checar_resultado() == 0 and impossivel() != -2:
         print("o jogo está indefinido")
         return -1
     return None
@@ -51,12 +51,15 @@ def impossivel():
     """Função que define que alguma regra do jogo foi violada"""
     qntd_o = np.count_nonzero(board == 1)
     qntd_x = np.count_nonzero(board == 2)
-    if abs(qntd_o - qntd_x) != 1:
+    if abs(qntd_o - qntd_x) == 0:
+        return 0
+    if abs(qntd_o - qntd_x) != 1 and np.sum(board) != 0:
         print("o jogo é impossível")
         return -2
     return None
 
 
-board = np.array([[2, 1, 1], [2, 1, 1], [2, 2, 1]])
-print(board)
-print(resultado())
+board = np.array([[1, 2, 1], [1, 2, 2], [2, 1, 1]])
+print(f'{board[0]}\n{board[1]}\n{board[2]}')
+
+resultado()
